97dfc91877a58cbf717c9f805a1a7c313d28e092
yangsheng <wu0306109@gmail.com>
2022-04-09 11:11:09 +0800

    Add upload interfaces.

--- model_manager/model_manager.py
+++ model_manager/model_manager.py
@@ -9,3 +9,38 @@ model_manager = Blueprint('website',
 @model_manager.route('/')
 def hello_world():
     return 'Hello World'
+
+
+@model_manager.route('/request-upload')
+def request_upload():
+    """Request a upload process.
+
+    Args:
+        name (str)
+        decription (str)
+
+    Return:
+        process_id (str)
+    """
+    pass
+
+
+@model_manager.route('/upload')
+def upload():
+    """Upload a file.
+
+    Args:
+        process_id (str)
+        file (File)
+    
+    Return:
+        success (bool)
+    """
+    import requests
+    requests.post('', params={}, files='')
+    pass
+
+
+@model_manager.route('/check-progress')
+def check_progress():
+    pass
