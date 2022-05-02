939aad8939f93b81240a822a946aaa9cca729e9e
yangsheng <wu0306109@gmail.com>
2022-05-03 02:00:33 +0800

    Remove repeated codes

--- tests/test_model_manager.py
+++ tests/test_model_manager.py
@@ -1,23 +1,2 @@
-import pytest
-from model_manager import app_creator
-
-app = app_creator.create_app()
-
-@pytest.fixture()
-def app():
-
-    app.config.update({
-        "TESTING": True,
-    })
-    yield app
-
-
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
-
 def test_request_upload(client):
     response = client.get('/')
-
-
