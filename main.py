6150fd43eb1a4bff778e3b09cee04f6f6643facb
yangsheng <wu0306109@gmail.com>
2022-05-02 09:51:57 +0800

    Remove Google logging module

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import os
 from typing import NoReturn
 
-import google.cloud.logging
-
 from model_manager import create_app
-
-# Retrieves a Cloud Loggin handler and intergrates with Python loggin module
-client = google.cloud.logging.Client()
-client.get_default_handler()
-client.setup_logging()
 
 
 def main() -> NoReturn:
