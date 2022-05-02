7807f9ab97e2cd52b54ad08c7c519e90de88b758
yangsheng <wu0306109@gmail.com>
2022-05-03 01:51:56 +0800

    Update port to fixed '8080'

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from typing import NoReturn
 
 from model_manager import create_app
 
+app = create_app()
+
 
 def main() -> NoReturn:
-    app = create_app()
-    app.run(debug=True,
-            host='0.0.0.0',
-            port=int(os.environ.get('PORT', '8080')))
+    app.run(debug=True, host='0.0.0.0', port='8080')
 
 
 if __name__ == '__main__':
