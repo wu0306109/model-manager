7d402a1c5afe4ca2ff3a036fcabce50f46e93937
yangsheng <wu0306109@gmail.com>
2022-05-04 03:44:29 +0800

    move flask initializer to `__init__.py`

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import os
 from typing import NoReturn
 
-import firebase_admin
-from firebase_admin import credentials
-
 from model_manager import create_app
-
-cred = credentials.Certificate(
-    'secrets/model-manager-349101-firebase-adminsdk-pzrld-ca1a04e3a6.json')
-firebase_admin.initialize_app(cred)
 
 app = create_app()
 
