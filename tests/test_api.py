35ff9a1d53ba73538979d673fb5790d0e95daf05
yangsheng <wu0306109@gmail.com>
2022-05-04 03:27:40 +0800

    move flask client to `test_api.py`

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,30 @@
+from collections.abc import Generator
+
+import pytest
+from flask import Flask
+from model_manager import create_app
+from werkzeug.test import Client
+
+
+@pytest.fixture()
+def app() -> Generator[Flask]:
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app) -> Client:
+    return app.test_client()
+
+
 def test_hello_world(client) -> None:
     response = client.get('/api/')
     assert response.data == b'Hello, World!'
