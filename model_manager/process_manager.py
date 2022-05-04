4be5208ac5ef042e672214d47a6b0461b33d05a4
yangsheng <wu0306109@gmail.com>
2022-05-04 19:30:09 +0800

    fix docker cannot build

--- model_manager/process_manager.py
+++ model_manager/process_manager.py
@@ -6,8 +6,6 @@ from distutils.command.upload import upload
 from pathlib import Path
 from typing import Any, NamedTuple
 
-from py import process
-
 from model_manager.process import UploadProcess, ProcessResultBase
 from model_manager.firebase_daos import UploadProcessDao, FileDao
 
@@ -27,13 +25,14 @@ import asyncio
 #     def describe(self) -> str:
 #         pass
 
+
 class ProcessManager():
 
     def __init__(self):
         # self.process_queue = list()
-        self.max_process_amount:int = 5
-        self.file_path:str = './data/temp/'
-        self.disk_path:str = '/'
+        self.max_process_amount: int = 5
+        self.file_path: str = './data/temp/'
+        self.disk_path: str = '/'
 
     def load_waiting_process_id(self) -> list:
         dao = UploadProcessDao()
@@ -47,14 +46,15 @@ class ProcessManager():
         dao = UploadProcessDao()
         return dao.get_all_finish_process_id()
 
-    def check_process_in_queue(self, process_id: str, process_queue: list) -> bool:
+    def check_process_in_queue(self, process_id: str,
+                               process_queue: list) -> bool:
         return process_id in process_queue
 
     def get_process_by_id(self, process_id: str) -> UploadProcess:
         dao = UploadProcessDao()
         upload_process = dao.get(process_id)
         return upload_process
-    
+
     # api upload file request
     def upload_file_request(self, file_name: str, description: str) -> str:
         if self.check_file_exist(file_name):
@@ -62,7 +62,8 @@ class ProcessManager():
         if not self.check_storage_has_space():
             return 'space is run out'
         new_process_id = self.generate_process_id()
-        new_process = self.create_process(new_process_id, self.file_path, file_name, description)
+        new_process = self.create_process(new_process_id, self.file_path,
+                                          file_name, description)
         self.save_new_process(new_process)
         return new_process_id
 
@@ -70,61 +71,64 @@ class ProcessManager():
         dao = UploadProcessDao()
         dao.add(process)
 
-    def check_file_exist(self, file_name: str) -> bool: #Todo need fix
+    def check_file_exist(self, file_name: str) -> bool:  #Todo need fix
         process_dao = UploadProcessDao()
         file_dao = FileDao()
         process_file_list = process_dao.get_all_file()
         file_list = file_dao.get_all_file_name_list()
-        if(file_name in file_list):
+        if (file_name in file_list):
             return True
-        if(file_name in process_file_list):
+        if (file_name in process_file_list):
             return True
         return False
 
     def check_storage_has_space(self) -> bool:
         total, used, free = shutil.disk_usage(self.disk_path)
-        free = free//(2 ** 30)
+        free = free // (2**30)
         waiting_process_id_list = self.load_waiting_process_id()
         files_size = 0
         for id in waiting_process_id_list:
             process = self.get_process_by_id(id)
             files_size += process.get_file_size()
-        files_size = files_size//(2 ** 30)
+        files_size = files_size // (2**30)
         #disk - running processes'size > 5G return True
         return True if (free - files_size) > 5 else False
 
     def generate_process_id(self) -> str:
         return str(uuid.uuid4())
 
-    def create_process(self, process_id: str, file_path: str, file_name: str, description: str) -> None:
+    def create_process(self, process_id: str, file_path: str, file_name: str,
+                       description: str) -> None:
         process = UploadProcess(process_id, file_path, file_name, description)
         return process
 
     # api set process stream
-    def transport_file(self, process_id: str, file_stream: LimitedStream, file_size: int) -> str:
+    def transport_file(self, process_id: str, file_stream: LimitedStream,
+                       file_size: int) -> str:
         waiting_process_id_list = self.load_waiting_process_id()
         running_process_id_list = self.load_running_process_id()
         finish_process_id_list = self.load_finish_process_id()
-        if(self.check_process_in_queue(process_id, running_process_id_list)):
+        if (self.check_process_in_queue(process_id, running_process_id_list)):
             return 'process is running'
-        if(self.check_process_in_queue(process_id, finish_process_id_list)):
+        if (self.check_process_in_queue(process_id, finish_process_id_list)):
             return 'process is finish'
-        if(not self.check_process_in_queue(process_id, waiting_process_id_list)):
+        if (not self.check_process_in_queue(process_id,
+                                            waiting_process_id_list)):
             return 'process does not exist'
-        if(len(running_process_id_list) >= self.max_process_amount):
+        if (len(running_process_id_list) >= self.max_process_amount):
             return 'wait busy'
-        
+
         process = self.get_process_by_id(process_id)
         process.set_stream(file_stream)
         process.set_file_size(file_size)
-        
-        status = self.launch_process(process) # TODO status
-        if(status):
+
+        status = self.launch_process(process)  # TODO status
+        if (status):
             return 'upload finish'
         else:
             self.reset_process()
             return 'upload fail'
-        
+
     def launch_process(self, process: UploadProcess):
         process.set_is_running(True)
         chunk_size = 1024
@@ -147,11 +151,11 @@ class ProcessManager():
                 self.save_process_status(process)
                 file.write(chunk)
             file.close()
-    
+
     def save_process_status(self, process: UploadProcess):
         dao = UploadProcessDao()
         dao.add(process)
-    
+
     def reset_process(self, process: UploadProcess):
         dao = UploadProcessDao()
         process.set_end_time(0)
@@ -166,12 +170,10 @@ class ProcessManager():
         waiting_process_id_list = self.load_waiting_process_id()
         running_process_id_list = self.load_running_process_id()
         finish_process_id_list = self.load_finish_process_id()
-        if(self.check_process_in_queue(process_id, running_process_id_list)):
+        if (self.check_process_in_queue(process_id, running_process_id_list)):
             process = self.get_process_by_id(process_id)
             return str(process.get_progress())
-        if(self.check_process_in_queue(process_id, finish_process_id_list)):
+        if (self.check_process_in_queue(process_id, finish_process_id_list)):
             return 'process is finish'
-        if(self.check_process_in_queue(process_id, waiting_process_id_list)):
+        if (self.check_process_in_queue(process_id, waiting_process_id_list)):
             return 'process is waiting'
-
-
